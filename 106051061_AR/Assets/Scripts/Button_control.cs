f920663dc844c3e063b8cba872892da48e4d91a4
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2020-11-17 03:10:10 +0800

    11/17

---
+++ 106051061_AR/Assets/Scripts/Button_control.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_control : MonoBehaviour
+{
+    public Text UItext;
+    private void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+       
+    }
+}
---
+++ 106051061_AR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("太空人")]
+    public Transform astro;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("移動速度")]
+    public float move = 0.01f;
+    [Header("旋轉速度")]
+    public float turn = 3f;
+    [Header("太空人動畫")]
+    public Animator aniastro;
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+        astro.Rotate(0, joystick.Horizontal*turn, 0);
+        astro.Translate(0,joystick.Vertical*move,0);
+        astro.transform.position = new Vector3(0,Mathf.Clamp(astro.transform.position.y,0.1f,20f),0);
+
+    }
+    public void Playanimation(string aniName)
+    {
+        aniastro.SetTrigger(aniName);
+        aniastro.SetBool(aniName, true);
+    }
+}
