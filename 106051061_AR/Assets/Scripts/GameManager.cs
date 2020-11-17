831e210db83e1a39144b283f4f2a961ad45b275f
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2020-11-17 11:29:00 +0800

    APK不能發佈

--- 106051061_AR/Assets/Scripts/GameManager.cs
+++ 106051061_AR/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
 {
     [Header("太空人")]
     public Transform astro;
+    public Transform trump;
     [Header("虛擬搖桿")]
     public FixedJoystick joystick;
     [Header("移動速度")]
@@ -13,6 +14,8 @@ public class GameManager : MonoBehaviour
     public float turn = 3f;
     [Header("太空人動畫")]
     public Animator aniastro;
+    [Header("川普動畫")]
+    public Animator anitrump;
 
     private void Start()
     {
@@ -23,13 +26,20 @@ public class GameManager : MonoBehaviour
         print("更新事件");
         print(joystick.Vertical);
         astro.Rotate(0, joystick.Horizontal*turn, 0);
+        trump.Rotate(0, joystick.Horizontal * turn, 0);
+
         astro.Translate(0,joystick.Vertical*move,0);
+        trump.Translate(0, joystick.Vertical * move, 0);
+
         astro.transform.position = new Vector3(0,Mathf.Clamp(astro.transform.position.y,0.1f,20f),0);
+      trump.transform.position = new Vector3(0, Mathf.Clamp(trump.transform.position.y, 0.1f, 20f), 0);
 
     }
     public void Playanimation(string aniName)
     {
         aniastro.SetTrigger(aniName);
+        anitrump.SetTrigger(aniName);
         aniastro.SetBool(aniName, true);
+        anitrump.SetBool(aniName, true);
     }
 }
